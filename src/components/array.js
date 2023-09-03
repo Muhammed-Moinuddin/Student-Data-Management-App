aea6543795fcc20fcc82d4c756cd8fe6e761656d
Muhammad Moinuddin <moinuddinilyas786@gmail.com>
2023-09-03 11:22:26 +0500

    Commit # 7
    Added some dummy data

--- src/components/array.js
+++ src/components/array.js
@@ -1,3 +1,11 @@
-const array = [ ]
+const array = [
+    {
+        id: 1,
+        name: 'Moin',
+        age: 21,
+        class: '3rd Year',
+        program: 'BSCS'
+    }
+ ]
 
 export default array;
